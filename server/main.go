7a611d4b82d224c10ae6f0c00cdd3109d975407e
Aidan Carey <aidancarey2004@protonmail.com>
2021-09-15 20:39:41 -0300

    comments

--- server/main.go
+++ server/main.go
@@ -18,9 +18,10 @@ type client struct {
     nick string
 }
 
-var clients = make([]client, 0)
+var clients = make([]client, 0) // slice of all connected clients
 
 func main() {
+    // start listening for connections
     ln, err := net.Listen("tcp", host+":"+port)
     if err != nil {
         fmt.Println("Error listening:", err.Error())
@@ -31,29 +32,33 @@ func main() {
     fmt.Println("Listening...")
 
     for {
+        // accept a connection
         conn, err := ln.Accept()
         if err != nil {
             fmt.Println("Error accepting:", err.Error())
             os.Exit(1)
         }
         
-        cl := client {conn, "nick"}
+        cl := client {conn, "nick"} // create new client struct for connection
 
-        clients = append(clients, cl)
+        clients = append(clients, cl) // add client to slice of all clients
         
         go handleConnection(cl)
     }
 }
 
 func handleConnection(cl client) {
+    // get name from client
     cl.conn.Write([]byte("Please enter your name: "))
     
-    name, _ := bufio.NewReader(cl.conn).ReadString('\n') 
+    name, _ := bufio.NewReader(cl.conn).ReadString('\n')
 
     name = strings.TrimSuffix(name, "\r\n")
     
     cl.conn.Write([]byte("Hello, " + name + ", how are you?\n"))
     
+    // TODO: update name in array
+
     cl.conn.Close()
 
     removeClient(cl)
